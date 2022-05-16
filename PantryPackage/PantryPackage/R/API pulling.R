d821ae2c545b74b698bea87a280a2a20db1be50f
EGabrilovica <94177132+EGabrilovica@users.noreply.github.com>
2022-05-16 15:07:01 +0200

    testing API & created a shiny app skeleton

--- PantryPackage/PantryPackage/R/API pulling.R
+++ PantryPackage/PantryPackage/R/API pulling.R
@@ -1,42 +1,30 @@
-install.packages("httr")
 library(httr)
 
 url <-
   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
 
-queryString <- list(
-  ingredients = "cucumber, spinach, tomato", # required parameter
+recipepull <- list(
+  ingredients = "chickpeas, spinach, tomato", # required parameter
   ranking = "1", # 1 = maximize used ingredients
   # 2 = minimize missing ingredients first
   ignorePantry = "true", #ignore salt, sugar, flour etc.
   number = "1" # number of recipes to return
 )
 
-response <- VERB("GET", url,
+pantryrecipe <- VERB("GET", url,
                  add_headers(
                    'X-RapidAPI-Host' =
                      'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
                    'X-RapidAPI-Key' =
                      'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
-                 query = queryString, content_type("application/octet-stream"))
+                 query = recipepull, content_type("application/octet-stream"))
 
-content(response, "parsed")
+output <- content(pantryrecipe, "parsed")
+id <- output[[1]][["id"]]
 
+# with every additional recipe, the function just creates another list
 
-summaryrecipe <-
-  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/281307/summary"
-
-responsesum <- VERB("GET", summaryrecipe,
-                 add_headers(
-                   'X-RapidAPI-Host' =
-                      'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-                   'X-RapidAPI-Key' =
-                     'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
-                 content_type("application/octet-stream"))
-
-content(responsesum, "text")
-
-
+# extracts recipe full info
 fullinfo <-
   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/281307/information"
 
@@ -48,10 +36,20 @@ responsefull <- VERB("GET", fullinfo,
                          'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
                      content_type("application/octet-stream"))
 
-content(responsefull, "text")
+recipeinfo <- content(responsefull, "parsed")
+
+
+# ASK FOR HELP W THE URL THING
 
 
 
-
+if(recipeinfo[["vegan"]] == "FALSE") { # poor building block for the if function
+  queryString <- list(
+    ingredients = "chickpeas, spinach, tomato",
+    ranking = "1",
+    ignorePantry = "true",
+    number = "2"
+  )
+}
 
 
