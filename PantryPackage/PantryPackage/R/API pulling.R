63e7444ef38617a4833d6d2b2ee593144f44d8b2
EGabrilovica <94177132+EGabrilovica@users.noreply.github.com>
2022-05-17 13:15:28 +0200

    added a while loop skeleton

--- PantryPackage/PantryPackage/R/API pulling.R
+++ PantryPackage/PantryPackage/R/API pulling.R
@@ -3,7 +3,8 @@ library(httr)
 url <-
   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
 
-recipepull <- list(
+# pulls recipe based on ingredients
+pantrycontent <- list(
   ingredients = "chickpeas, spinach, tomato", # required parameter
   ranking = "1", # 1 = maximize used ingredients
   # 2 = minimize missing ingredients first
@@ -17,12 +18,11 @@ pantryrecipe <- VERB("GET", url,
                      'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
                    'X-RapidAPI-Key' =
                      'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
-                 query = recipepull, content_type("application/octet-stream"))
+                 query = pantrycontent, content_type("application/octet-stream"))
 
-output <- content(pantryrecipe, "parsed")
-id <- output[[1]][["id"]]
-
-# with every additional recipe, the function just creates another list
+pulledrecipe <- content(pantryrecipe, "parsed")
+recipenumber <- length(pulledrecipe) # the number of recipes
+id <- pulledrecipe[[recipenumber]][["id"]] # obtains recipe id
 
 # extracts recipe full info
 fullinfo <-
@@ -42,14 +42,48 @@ recipeinfo <- content(responsefull, "parsed")
 # ASK FOR HELP W THE URL THING
 
 
+while(recipeinfo[[diet]] == FALSE) { # for now it can update the recipe pulled
+  numrecipe <- 1
 
-if(recipeinfo[["vegan"]] == "FALSE") { # poor building block for the if function
-  queryString <- list(
-    ingredients = "chickpeas, spinach, tomato",
+  pantrycontent <- list(
+    ingredients = ingredient, # as named in the shiny app
     ranking = "1",
     ignorePantry = "true",
-    number = "2"
+    number = as.character(numrecipe)
   )
+
+  pantryrecipe <- VERB("GET", url,
+                       add_headers(
+                         'X-RapidAPI-Host' =
+                           'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+                         'X-RapidAPI-Key' =
+                           'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
+                       query = pantrycontent, content_type("application/octet-stream"))
+
+  pulledrecipe <- content(pantryrecipe, "parsed")
+  id <- pulledrecipe[[numrecipe]][["id"]]
+
+  fullinfo <-
+    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/281307/information"
+
+  responsefull <- VERB("GET", fullinfo,
+                       add_headers(
+                         'X-RapidAPI-Host' =
+                           'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+                         'X-RapidAPI-Key' =
+                           'b729bf5802mshd4d7b8537e441f3p1850d9jsnc8b828bd5f3a'),
+                       content_type("application/octet-stream"))
+
+  recipeinfo <- content(responsefull, "parsed")
+  numrecipe <- numrecipe + 1
 }
+
+if(recipeinfo[[diet]] == "TRUE") { # if dietary restrictions match
+ print(recipeinfo[["title"]]),
+  print(recipeinfo[["extendedIngredients"]]),
+  print(recipeinfo[["instructions"]])
+}
+
+
 
 
