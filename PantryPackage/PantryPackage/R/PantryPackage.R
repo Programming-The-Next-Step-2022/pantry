0b60d52cc830d1ad49cad7f93db261a8299b2f59
EGabrilovica <94177132+EGabrilovica@users.noreply.github.com>
2022-05-25 12:04:56 +0200

    documented the recipe information function

---
+++ PantryPackage/PantryPackage/R/PantryPackage.R
@@ -0,0 +1,15 @@
+#' This package produces recipes based on what food you already have
+#'
+#' The package contains two functions: 1) get_id() and 2) get_info()
+#'
+#' @section Get ID:
+#' \emph{get_id} produces ID numbers of recipes based on an ingredient list
+#' input
+#'
+#' @section Recipe Info:
+#' \emph{get_info} produces full recipe information based on input of ingredients
+#'  and dietary restrictions
+#'
+#' @docType package
+#' @name Pantry
+NULL
