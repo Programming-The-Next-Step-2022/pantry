251ccefb43208479f2d251d619ce7e046c00bdd0
EGabrilovica <94177132+EGabrilovica@users.noreply.github.com>
2022-05-26 14:49:31 +0200

    working on the shiny app and markdown

--- PantryPackage/PantryPackage/whats_in_my_pantry/app.R
+++ PantryPackage/PantryPackage/whats_in_my_pantry/app.R
@@ -1,19 +1,33 @@
 #shiny app for the pantry function
 
 library(shiny)
+library(PantryPackage)
 
-diets <- list("vegetarian", "vegan", "gluten free", "dairy free")
+diets <- c("None", "Pescatarian" ,"Vegetarian", "Vegan", "Gluten Free",
+              "Dairy Free", "Soy Free", "Pork Free")
 
 ui <- fluidPage(
-            textInput("ingredient", "What do you have?",
-                      placeholder = "e.g., tomato"), # ingredient input
-            selectizeInput("diet", "dietary restrictions?",
-                           choices = diets, allowEmptyOption = TRUE),#diet input
-            textOutput("recipe")
-        )
+    title = "What's in the pantry?",
+    textInput("ingredient", "What do you have?",
+              placeholder = "e.g., tomato"), # ingredient input
+    selectInput("diet", "dietary restrictions?",
+                choices = diets), # diet input
+    textOutput("recipe")
+    )
 
 server <- function(input, output) {
 
+  recipeinfo <- reactive({
+    food <- renderText(strsplit(input$ingredient, ",\\s*")[[1]])
+    restr <- if (input$diet == "None") {
+      restr <- NULL
+    } else {
+      renderText(as.character(input$diet))
+    }
+    return(get_info(ingredients == food, diet == restr))
+  })
+
+  output$recipe <- renderUI({recipeinfo})
   }
 
 # Run the application
