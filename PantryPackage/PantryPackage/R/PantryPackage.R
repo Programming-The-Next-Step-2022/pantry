9a0f6ae57cb8ac845e9345c036f36da1d2abeb71
EGabrilovica <94177132+EGabrilovica@users.noreply.github.com>
2022-06-02 11:52:58 +0200

    Updated package information and changed labels for the rmd doc

--- PantryPackage/PantryPackage/R/PantryPackage.R
+++ PantryPackage/PantryPackage/R/PantryPackage.R
@@ -1,10 +1,14 @@
 #' This package produces recipes based on what food you already have
 #'
-#' The package contains one function: get_info()
+#' The package contains two functions: get_info() and whatsinmypantry()
 #'
 #' @section Recipe Info:
-#' \emph{get_info} produces full recipe information based on input of ingredients
-#'  and dietary restrictions
+#' \emph{get_info} produces full recipe information based on input of
+#' ingredients and dietary restrictions
+#'
+#' @section What's in my pantry:
+#' \emph{whatsinmypantry} launches the "What's in my pantry?" shiny app in a
+#' browser window
 #'
 #' @docType package
 #' @name Pantry
